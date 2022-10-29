4d7b49e4ff8a9276529a8afd162c7e1d8a92d819
luisdi28 <luisdi.28@estudiantec.cr>
2022-10-29 12:37:54 -0600

    Commit pruebas unitarias

--- tst/main.cpp
+++ tst/main.cpp
@@ -1,57 +1,56 @@
 #include "opencv4/opencv2/imgcodecs.hpp"
 #include "opencv4/opencv2/highgui.hpp"
+#include <opencv4/opencv2/opencv.hpp>
 #include <iostream>
 #include "gtest/gtest.h"
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <iostream>
 
-// we're NOT "using namespace std;" here, to avoid collisions between the beta variable and std::beta in c++17
-using std::cin;
-using std::cout;
-using std::endl;
 using namespace cv;
+using namespace std;
 
 class Server{
 
 public:
+    int método()
+    {
+        // Read the image file
+        Mat image = imread("/home/luis/CLionProjects/Proyecto 1 - Datos II/gray_scale/tst/cuadras.jpg");
 
-    int método(int argc, char **argv) {
-        CommandLineParser parser(argc, argv,"{@input | /home/luis/CLionProjects/Proyecto 1 - Datos II/Gamma_Correction/cuadras.jpg | input image}");
-        Mat image = imread(samples::findFile(parser.get<String>("@input")));
-        if (image.empty()) {
-            cout << "Could not open or find the image!\n" << endl;
-            cout << "Usage: " << argv[0] << " <Input image>" << endl;
+        // Check for failure
+        if (image.empty())
+        {
+            cout << "Could not open or find the image" << endl;
+            cin.get(); //wait for any key press
             return -1;
         }
 
-    }
+        //Blur the image with 3x3 Gaussian kernel
+        Mat image_blurred_with_3x3_kernel;
+        GaussianBlur(image, image_blurred_with_3x3_kernel, Size(3, 3), 0);
 
-    void metodo2(){
-        Mat new_image = Mat::zeros( image.size(), image.type() );
-        double alpha = 1.0; /*< Simple contrast control */
-        int beta = 0;       /*< Simple brightness control */
-        cout << " Basic Linear Transforms " << endl;
-        cout << "-------------------------" << endl;
-        cout << "* Enter the alpha value [1.0-3.0]: "; cin >> alpha;
-        cout << "* Enter the beta value [0-100]: ";    cin >> beta;
-        for( int y = 0; y < image.rows; y++ ) {
-            for( int x = 0; x < image.cols; x++ ) {
-                for( int c = 0; c < image.channels(); c++ ) {
-                    new_image.at<Vec3b>(y,x)[c] =
-                            saturate_cast<uchar>( alpha*image.at<Vec3b>(y,x)[c] + beta );
-                }
-            }
-        }
-        imshow("Original Image", image);
-        imshow("New Image", new_image);
-        waitKey();
+        //Blur the image with 5x5 Gaussian kernel
+        Mat image_blurred_with_5x5_kernel;
+        GaussianBlur(image, image_blurred_with_5x5_kernel, Size(5, 5), 0);
+
+        //Define names of the windows
+        String window_name = "Lotus";
+        String window_name_blurred_with_3x3_kernel = "Lotus Blurred with 3 x 3 Gaussian Kernel";
+        String window_name_blurred_with_5x5_kernel = "Lotus Blurred with 5 x 5 Gaussian Kernel";
+
+        // Create windows with above names
+        namedWindow(window_name);
+        namedWindow(window_name_blurred_with_3x3_kernel);
+        namedWindow(window_name_blurred_with_5x5_kernel);
+
+        // Show our images inside the created windows.
+        imshow(window_name, image);
+        imshow(window_name_blurred_with_3x3_kernel, image_blurred_with_3x3_kernel);
+        imshow(window_name_blurred_with_5x5_kernel, image_blurred_with_5x5_kernel);
+
+        waitKey(0); // Wait for any keystroke in the window
+
+        destroyAllWindows(); //destroy all opened windows
+
         return 0;
-    }
     }
 };
 
